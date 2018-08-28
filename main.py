c99ab866852ca45fbc0cd22c7c027feab9ee0da6
arunjayaprakash <arun77733@gmail.com>
2018-08-28 10:09:59 +0530

    added backward elim summary

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ Created on Sun Aug 19 10:12:43 2018
 @author: arunjp
 """
 import pandas as pd
+import numpy as np
 
-
-dataset = pd.read_csv('/home/arunjp/Work/parkinsons/new/consolidated.csv')
+dataset = pd.read_csv('consolidated.csv')
 #---------------------LINEAR REGRESSION--------------------------------------------------------------------------------
 X = dataset.drop(['BirthYear','DiagnosisYear','Parkinsons'], axis =1)
 X = X.iloc[:,1:].values
@@ -24,7 +24,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Logistic Regression to the Training set
+# Fitting LOGISTIC REGRESSOR to the Training set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
@@ -36,3 +36,10 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# BACKWARD ELIMINATION
+import statsmodels.formula.api as sm
+X = np.append(arr=np.ones((217,1)).astype(int), values= X, axis=1)
+X_opt = X[:,:]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
