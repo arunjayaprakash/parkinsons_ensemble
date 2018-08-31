b842eb330355f258f1de080f1aadfd437aef250e
arunjayaprakash <arun77733@gmail.com>
2018-08-31 16:37:09 +0530

    lda

--- main_lda.py
+++ main_lda.py
@@ -9,13 +9,13 @@ import pandas as pd
 import numpy as np
 
 dataset = pd.read_csv('/home/arunjp/Work/parkinsons/ensemble/consolidated.csv')
-#---------------------LINEAR REGRESSION--------------------------------------------------------------------------------
+
 X = dataset.drop(['BirthYear','DiagnosisYear','Parkinsons'], axis =1)
 X = X.iloc[:,1:].values
 y = dataset.iloc[:,2].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -23,6 +23,18 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+
+
+
+
+
+
+#---------------------LDA-------------------------------------------
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components=2)
+X_train = lda.fit_transform(X_train,y_train)
+X_test = lda.transform(X_test)
 
 # Fitting LOGISTIC REGRESSOR to the Training set
 from sklearn.linear_model import LogisticRegression
@@ -32,14 +44,10 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# BACKWARD ELIMINATION
-import statsmodels.formula.api as sm
-X = np.append(arr=np.ones((217,1)).astype(int), values= X, axis=1)
-X_opt = X[:,:]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
+
+
+
